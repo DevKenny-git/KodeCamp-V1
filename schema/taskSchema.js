0e5152b4ef907e8f490aa8a968b82c7b2e744ec0
Shodipe Oladele <quickly2kenny@gmail.com>
2023-09-26 16:15:47 +0100

    Corrected error in taskSchema

--- schema/taskSchema.js
+++ schema/taskSchema.js
@@ -12,7 +12,7 @@ const taskSchema = new mongoose.Schema({
     pictureName: {
         type: String,
         default: ""
-    }
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "users"
