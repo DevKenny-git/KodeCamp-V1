c46926d7e259431c3b17594b477bdc9954668a42
Shodipe Oladele <quickly2kenny@gmail.com>
2023-09-26 16:01:39 +0100

    Added uploadPic middleware

---
+++ routes/uploadPics.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({dest: "public/"})
+const {taskCollection} = require("../schema/taskSchema");
+const {isUserLoggedIn} = require("./middlewares");
+
+
+
+
+app.use(isUserLoggedIn);
+
+router.post("/pic", upload.single("file"), async (req, res) => {
+    const {taskTitle, taskBody} = req.body;
+    const {filename} = req.file
+    
+    const newTask = await taskCollection.create({
+        taskTitle, taskBody, pictureName: filename
+    });
+    res.send({
+        Successful: true,
+        newTask
+    })
+});
+
+
+module.exports = router;
