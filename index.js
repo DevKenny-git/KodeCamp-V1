60f545f86329e258dc7693f2b6cf8efab6086ebc
Shodipe Oladele <quickly2kenny@gmail.com>
2023-09-26 17:11:17 +0100

    saved new changes to index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const taskRoute = require("./routes/tasks")
 const authRoute = require("./routes/auth")
+const path = require("path");
+const uploadPic = require("./routes/uploadPics")
 
 const connect = mongoose.connect(process.env.mongoDBURL)
 
@@ -16,9 +18,11 @@ connect.then(() => {
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/v1/tasks", taskRoute);
 app.use("/v1/auth", authRoute);
+app.use("/v1/upload-pic", uploadPic);
 
 
 app.listen(port, function() {
